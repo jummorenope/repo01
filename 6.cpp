d64203d5237d657f642d8ce06ce89e1bf5eee7c9
jummorenope <jummorenope@unal.edu.co>
2017-08-04 10:27:41 -0500

    grafica

--- 6.cpp
+++ 6.cpp
@@ -6,7 +6,7 @@ int main (void)
   std::cout.precision(16);
   std::cout.setf(std::ios::scientific);
   double sum=0;
-  for(double ii=1; ii<1000000; ii++)
+  for(double ii=1; ii<100000; ii++)
     {
       sum+=1/ii;
       std::cout<<ii<<"\t"<<sum<<std::endl;
