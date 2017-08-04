b27641c342c90883117a8a3da34ed6e8f6fa7756
jummorenope <jummorenope@unal.edu.co>
2017-08-04 09:49:30 -0500

    Final quinto punto

--- 5.cpp
+++ 5.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <cmath>
 
+bool primo(int n);
+
 int main (void)
 {
+  for(int ii=500; ii<=1400; ii++)
+    {
+      if(primo(ii)==true)
+	std::cout<<ii<<std::endl;
+    }
   return 0;
+}
+
+bool primo(int n)
+{
+  int r=std::sqrt(n);
+  for(int ii=2; ii<=r; ii++)
+    {
+      if(n%ii==0)
+	return true;
+    }
+  return false;
 }
