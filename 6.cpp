6451a8523fa2f76d89c520b1deda922e847c08f3
jummorenope <jummorenope@unal.edu.co>
2017-08-04 09:56:25 -0500

    Sexto punto

---
+++ 6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main (void)
+{
+  std::cout.precision(16);
+  std::cout.setf(std::ios::scientific);
+  long double sum=0;
+  for(int ii=1; ii<1000000; ii++)
+    {
+      sum+=1/ii;
+      std::cout<<ii<<"\t"<<sum<<std::endl;
+    }
+  return 0;
+}
