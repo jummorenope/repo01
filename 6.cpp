4f1d195a24f501ffd7c0794302d5b8addf81ebfd
jummorenope <jummorenope@unal.edu.co>
2017-08-04 10:08:15 -0500

    Segunda correcion sexto punto

--- 6.cpp
+++ 6.cpp
@@ -5,7 +5,7 @@ int main (void)
 {
   std::cout.precision(16);
   std::cout.setf(std::ios::scientific);
-  long double sum=0;
+  double sum=0;
   for(double ii=1; ii<1000000; ii++)
     {
       sum+=1/ii;
